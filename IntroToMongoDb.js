4a1adfec07472af4fbde19eaf8554e78c36f0fae
Jatin <jatin8085vashisht@gmail.com>
2022-03-02 00:18:19 +0530

    created database

--- IntroToMongoDb.js
+++ IntroToMongoDb.js
@@ -1,1 +1,9 @@
-import {mongo} from 'mongodb';
+import mongo from 'mongodb';
+import { MongoClient } from 'mongodb';
+var url = "mongodb://127.0.0.1:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
