27578d26355582ab7787052205d972665cb1b6f4
Jatin <jatin8085vashisht@gmail.com>
2022-03-02 00:32:38 +0530

    created collection

--- IntroToMongoDb.js
+++ IntroToMongoDb.js
@@ -2,8 +2,16 @@ import mongo from 'mongodb';
 import { MongoClient } from 'mongodb';
 var url = "mongodb://127.0.0.1:27017/mydb";
 
+const databaseName = "mydb"
+
+// Important: In MongoDB, a database is not created until it gets content!
+// a collection in mongodb is same as table in sql
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("Database created!");
-  db.close();
+  let dbo = db.db(databaseName)
+  dbo.createCollection("customers", function(err, res){
+    if(err) throw err
+    console.log("collection created")
+    db.close();
+  })
 });
