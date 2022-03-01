44248381eb1504ea445460aa4cd61c817b3b912b
Jatin <jatin8085vashisht@gmail.com>
2022-03-02 01:01:06 +0530

    inserting many documents to collection

--- IntroToMongoDb.js
+++ IntroToMongoDb.js
@@ -14,8 +14,21 @@ const databaseName = "mydb"
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   let dbo = db.db(databaseName)
-  let myObj = {name: "Company Inc", address: "Highway 37"};
-  dbo.collection("customers").insertOne(myObj, function(err, res){
+  let myObj = [{ name: 'John', address: 'Highway 71'},
+    { name: 'Peter', address: 'Lowstreet 4'},
+    { name: 'Amy', address: 'Apple st 652'},
+    { name: 'Hannah', address: 'Mountain 21'},
+    { name: 'Michael', address: 'Valley 345'},
+    { name: 'Sandy', address: 'Ocean blvd 2'},
+    { name: 'Betty', address: 'Green Grass 1'},
+    { name: 'Richard', address: 'Sky st 331'},
+    { name: 'Susan', address: 'One way 98'},
+    { name: 'Vicky', address: 'Yellow Garden 2'},
+    { name: 'Ben', address: 'Park Lane 38'},
+    { name: 'William', address: 'Central st 954'},
+    { name: 'Chuck', address: 'Main Road 989'},
+    { name: 'Viola', address: 'Sideway 1633'}];
+  dbo.collection("customers").insertMany(myObj, function(err, res){
     if(err) throw err;
     console.log("1 document inserted");
     db.close();
