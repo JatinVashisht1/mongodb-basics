7b8eb4be8be0ac473627c5d73a557ff10706043c
Jatin <jatin8085vashisht@gmail.com>
2022-03-02 12:58:49 +0530

    querying in mongo db

--- IntroToMongoDb.js
+++ IntroToMongoDb.js
@@ -13,63 +13,28 @@ const databaseName = "mydb"
 
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
-  var dbo = db.db(databaseName)
-
-  // Find One 🙂
-  // firstOne returns the first occurence in the selection
-  // the first parameter in the findOne function is a query parameter
-  // if we give empty query object to the query parameter then it selects all documents and returns the first one
-  dbo.collection("customers").findOne({}, function (err, res) {
-    if (err) throw err;
-    console.log("find one executed");
-    // res is the result object and it returns info about document inserted
-    // an example is below statement
-    console.log(`result is ${res.name}`);
-    // db.close();
-  });
-
-  // Find All 🙂
-  // find is used to select all the occurences of the result
-  // if we provide empty query parameter then it will return all the documents
-  dbo.collection("customers").find({}).toArray(function (err, res) {
-    if (err) throw err;
-    console.log(res);
-    // db.close();
-  });
-
-  // Find Some 🙂
-  // The second parameter of the find() method is the projection object that describes which fields to include in the result.
-  // This parameter is optional, and if omitted, all fields will be included in the result.
-
-  // in projection 0 meanse we don't want to include that field and 1 means we want to include that
-  dbo.collection("customers").find({}, { projection: { _id: 0, name: 1, address: 1 } }).toArray(function (err, res) {
-    if (err) throw err;
-    console.log(res);
-
-    // As you can see from the result of the example above, the result can be converted into an array containing each document as an object.
-
-    console.log("2nd element of array is: ",res[2].address, "\n--------------\n");
-    // db.close();
-  })
-
-  
-
-  // To exclude the _id field, you must set its value to 0
-  dbo.collection("customers").find({}, { projection: { name: 1 } }).toArray(function (err, res) {
-    if (err) throw err;
-    console.log(res);
-    db.close();
-  })
-
-
-  // You get an error if you specify both 0 and 1 values in the same object (except if one of the fields is the _id field)
-  /*
-  dbo.collection("customers").find({}, {projection :{name: 1, address:0}}).toArray(function(err, res){
+  var dbo = db.db(databaseName);
+  // Filter the Result 🙂
+  //   When finding documents in a collection, you can filter the result by using a query object.
+  // The first argument of the find() method is a query object, and is used to limit the search.
+  var query = { address: "Park Lane 38" };
+  dbo.collection("customers").find(query).toArray(function(err, res){
     if(err) throw err;
     console.log(res);
-    db.close();
-  })
-  */
+    // db.close();
+  });
 
+  // Filter with Regular Expressions 🙂
+  // Reqular expressions can only be used to query strings,
+  // You can write regular expressions to find exactly what you are searching for.
+
+  // this will return all the addresses starting with 'S'
+  query = { address: /^S/ };
+  dbo.collection("customers").find(query, {projection: {_id:0}}).toArray(function(err, res){
+    if(err) throw err;
+    // JSON.stringigy(*js array*) converts "js array" to JSON object 🔥
+    console.log(JSON.stringify(res));
+    db.close();
+  });
 
 });
